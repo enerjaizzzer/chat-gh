47d75a800687a4713b07aca22d6fb10551799492
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-15 00:25:47 +0300

    for relise

---
+++ precache-manifest.146e1d0bdf838a133839b9b99674425f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923b47d2080e2114b0c5062bddd39e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d9a5047e81f206d1c0",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6c061a7f79775f327945",
+    "url": "/static/css/main.3599cb3b.chunk.css"
+  },
+  {
+    "revision": "36d9a5047e81f206d1c0",
+    "url": "/static/js/2.5243fc93.chunk.js"
+  },
+  {
+    "revision": "6c061a7f79775f327945",
+    "url": "/static/js/main.91a9cf78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
